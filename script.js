09df49b773d4357f6942d4ee8c82a84427e1af8c
julcontrerass <111258374+julcontrerass@users.noreply.github.com>
2023-10-12 16:59:13 -0300

    add icon and edit about me

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
 //Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
-    var opciones = document.querySelectorAll('#links  a');
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
-    link.className = "seleccionado";
+  var opciones = document.querySelectorAll("#links  a");
+  opciones[0].className = "";
+  opciones[1].className = "";
+  opciones[2].className = "";
+  opciones[3].className = "";
+  opciones[4].className = "";
+  link.className = "seleccionado";
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-    //en modo responsive
-    var x = document.getElementById("nav");
-    x.className = "";
+  //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+  //en modo responsive
+  var x = document.getElementById("nav");
+  x.className = "";
 }
 
 //función que muestra el menu responsive
 function responsiveMenu() {
-    var x = document.getElementById("nav");
-    if (x.className === "") {
-        x.className = "responsive";
-    } else {
-        x.className = "";
-    }
+  var x = document.getElementById("nav");
+  if (x.className === "") {
+    x.className = "responsive";
+  } else {
+    x.className = "";
+  }
 }
 
 //detecto el scrolling para aplicar la animación del la barra de habilidades
-window.onscroll = function() { efectoHabilidades() };
+window.onscroll = function () {
+  efectoHabilidades();
+};
 
 //funcion que aplica la animación de la barra de habilidades
 function efectoHabilidades() {
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if (distancia_skills >= 300) {
-        document.getElementById("back").classList.add("barra-progreso1");
-        document.getElementById("front").classList.add("barra-progreso2");
-        document.getElementById("datos").classList.add("barra-progreso3");
-        
-    }
-
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    document.getElementById("back").classList.add("barra-progreso1");
+    document.getElementById("front").classList.add("barra-progreso2");
+    document.getElementById("datos").classList.add("barra-progreso3");
+  }
 }
 
 const form = document.querySelector("#contact-form");
@@ -47,15 +48,19 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
 
-  emailjs.send('service_kunp9jq', 'template_j1sxoa3', {
-    from_name: formData.get('name'),
-    from_email: formData.get('email'),
-    message: formData.get('message')
-  })
-    .then((response) => {
-      status.textContent = "Mensaje enviado correctamente";
-      form.reset();
-    }, (error) => {
-      status.textContent = error.message;
-    });
+  emailjs
+    .send("service_kunp9jq", "template_j1sxoa3", {
+      from_name: formData.get("name"),
+      from_email: formData.get("email"),
+      message: formData.get("message"),
+    })
+    .then(
+      (response) => {
+        status.textContent = "Mensaje enviado correctamente";
+        form.reset();
+      },
+      (error) => {
+        status.textContent = error.message;
+      }
+    );
 });
